353309c6d458d4d34405a25f34218ade0b714348
baaaavi <bladimarvillegas@gmail.com>
2024-05-19 18:51:38 -0300

    agregando javascript y cambios a la pagina

---
+++ micodigo.js
@@ -0,0 +1,21 @@
+let nombre = "Bladimar";
+let apellido = "Villegas";
+
+console.log(nombre + " " + apellido);
+
+let numeroa = 17;
+let numerob = 33;
+let numeroc = 14;
+let sumatoria; 
+let resultado;
+
+function suma(){
+    sumatoria = numeroa + numerob;
+    resultado = sumatoria + numeroc;
+}
+
+suma();
+
+console.log(sumatoria)
+
+console.log(resultado)
